2cb011840ef6da74f74af1f7529900395320bb27
Aleksandr Loginov <trashloginov@gmail.com>
2017-10-06 16:26:12 +0300

    tmp

--- src/app/trade/trade.component.ts
+++ src/app/trade/trade.component.ts
@@ -1,7 +1,7 @@
 import { compact } from 'lodash';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ServiceComponent } from '../service/service.component';
-// import * as Globals from '../globals/globals.component';
+import * as Globals from '../globals/globals.component';
 // import * as MainOpr from '../mainoperation/mainoperation.component';
 
 // flex-layout
@@ -11,9 +11,12 @@ import { ServiceComponent } from '../service/service.component';
 // import {Observable} from 'rxjs/Observable';
 
 import * as moment from 'moment';
+import {ActivatedRoute} from '@angular/router';
+import {ObservableMedia} from "@angular/flex-layout";
+import {Observable} from "rxjs/Observable";
 
-// let HttpService: any;
-// let self: any;
+let HttpService: any;
+let self: any;
 
 @Component({
   selector: 'app-trade',
@@ -22,10 +25,122 @@ import * as moment from 'moment';
   providers: [ServiceComponent]
 })
 export class TradeComponent implements OnInit, OnDestroy {
-    constructor() {}
+    private routeName: string;
+    private sub: any;
 
+    public isValid: boolean = false;
+    public cols:  Observable<number>;
 
-    ngOnInit() {}
+    // slider and datepicker attr
+    public ngSliderIndex = 0;
+    public disabled = false;
+    public maxVal = 0;
+    public minVal = 0;
+    public ng_strDate = Globals.convertDate(Globals.g_GlobalStatic.startDate);
+    public ngDatepicker = new Date(Globals.g_GlobalStatic.startDate);
+    // ngAllRefresh: number = 0;
+    // ngFileUploadPath: any;
+    public nTimerId: any;
 
-    ngOnDestroy() {}
+    constructor( private route: ActivatedRoute,
+                 private service:ServiceComponent,
+                 private observableMedia: ObservableMedia ) {
+        self = this;
+        HttpService = this.service;
+
+        this.sub = route.params.subscribe(params => {
+            this.routeName = params['name'];
+        });
+    }
+
+    ngOnInit() {
+        HttpService.getDatabaseInfo();
+        this.nTimerId = setInterval(() => {
+            console.log('Test ', Globals.g_DatabaseInfo);
+            //this.checkStart();
+        }, 100);
+
+        this.isValid = true;
+        const grid = new Map([
+            ['xs', 1],
+            ['sm', 1],
+            ['md', 1],
+            ['lg', 3],
+            ['xl', 3]
+        ]);
+        let start: number;
+        grid.forEach((cols, mqAlias) => {
+            if (this.observableMedia.isActive(mqAlias)) {
+                start = cols;
+            }
+        });
+        this.cols = this.observableMedia.asObservable()
+            .map(change => grid.get(change.mqAlias))
+            .startWith(start);
+
+        console.log('Test ', Globals.g_DatabaseInfo);
+        // this.setSlider();
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    // checkStart() {
+    //     if (Globals.g_DatabaseInfo.bIsStartCalc){
+    //         clearInterval(this.nTimerId);
+    //         MainOpr.onCalculateData();
+    //         HttpService.getTransactionList().subscribe(
+    //             response => {
+    //                 MainOpr.getTransactionData(response);
+    //                 MainOpr.CalculatePortfolioData();
+    //
+    //                 this.setSlider();
+    //                 this.onInitSelect();
+    //                 this.onPfnameChanged();
+    //                 this.setEscojePortafolio();
+    //                 this.setEscojeFondo();
+    //                 this.setComprarVender();
+    //                 this.onInitGraphData();
+    //                 this.onRefreshTable();
+    //                 this.checkTable();
+    //                 this.isValid = true;
+    //             });
+    //     }
+    // };
+
+    setSlider() {
+        this.minVal = 0;
+        this.maxVal = Globals.g_DatabaseInfo.ListofPriceFund[0].ulen - 1;
+    }
+
+    onInputChange(event: any) {
+        const updatedDate = new Date(Globals.g_GlobalStatic.startDate);
+        const selectedDate = updatedDate.setDate(updatedDate.getDate() + event.value);
+        this.ng_strDate = Globals.convertDate(selectedDate);
+        Globals.g_Portfolios.nSliderIndex = event.value;
+        this.ngDatepicker = new Date(selectedDate);
+
+        // Update Slider Index for send Event
+        this.ngSliderIndex = event.value;
+
+        // Update Escoje Fondo&Portafolio
+        // this.setEscojePortafolio();
+        // this.setEscojeFondo();
+        // this.setComprarVender();
+    }
+
+    onInputDatepicker(event: any) {
+        const diffDate = moment(event.value).diff(moment(Globals.g_GlobalStatic.startDate), 'days');
+        this.ng_strDate = Globals.convertDate(moment(event.value).format('YYYY-DD-MM'));
+        Globals.g_Portfolios.nSliderIndex = diffDate;
+
+        // Update Slider Index for send Event
+        this.ngSliderIndex = diffDate;
+
+        // Update Escoje Fondo&Portafolio
+        // this.setEscojePortafolio();
+        // this.setEscojeFondo();
+        // this.setComprarVender();
+    }
 }
