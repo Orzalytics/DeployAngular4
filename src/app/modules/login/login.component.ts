0ee9e4de68e39649b122686c16e1c9c2a18de3d1
Aleksandr Loginov <trashloginov@gmail.com>
2017-11-24 16:52:10 +0200

    added login form

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl('username', Validators.required),
+    password: new FormControl('password', Validators.required),
+  });
 
   constructor() { }
 
